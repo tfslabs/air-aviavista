70705267120ff76b8b80c701377d7da6a5329b54
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-13 20:05:44 +0700

    Update create-random-users.py
    Signed-off-by:  ねこ 白い <shiroineko.tfs@gmail.com>

--- azure-deployments/azure-ad/create-random-users.py
+++ azure-deployments/azure-ad/create-random-users.py
@@ -4,9 +4,9 @@ class personel_roles:
     def __init__(self):
         self.roles = [
             ['Captain', 'First Officer', 'Second Officer', 'Flight Attendant'],
-            ['Check-in Personel', 'Cargo Loader'],
-            ['Inspector', 'Maintaincer'],
-            ['Flight Scheduling Manager', 'Customer Service', 'Advertiment'],
+            ['Check-in Personel', 'Payload Manager'],
+            ['Inspector', 'Maintainer'],
+            ['Flight Scheduling Manager', 'Customer Service', 'Advertisement Manager'],
             ['Finance Operator', 'Human Resources']
         ]
         self.role_flight_attendants = self.roles[0]

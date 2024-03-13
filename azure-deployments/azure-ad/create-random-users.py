7df173be977442398a9f949d1886bb8def4ff5c4
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-13 22:42:41 +0700

    Update create-random-users.py

--- azure-deployments/azure-ad/create-random-users.py
+++ azure-deployments/azure-ad/create-random-users.py
@@ -7,9 +7,7 @@ import names, string, secrets
 class basic_info:
     
     def generate_password(self, length=randint(10, 16)):
-        # Define characters to use in the password
         characters = string.ascii_letters + string.digits + string.punctuation
-        # Generate random password
         password = ''.join(secrets.choice(characters) for _ in range(length))
         return password
     
@@ -24,20 +22,72 @@ class basic_info:
 
 class employment_info:
     
-    
-    def job_title(self):
-        roles = [
-            ['Captain', 'First Officer', 'Second Officer', 'Flight Attendant'],
-            ['Check-in Personel', 'Customer Service'],
-            ['Inspector', 'Maintainer'],
-            ['Flight Scheduling Manager', 'Payload Manager'],
-            ['Finance Operator', 'Human Resources', 'Advertisement Manager']
+    def usageLocation(self, countryNumber):
+        country = [
+            'United States',
+            'United Kingdom',
+            'France'
         ]
-        flight_attendants = roles[0][randint(0, 3)]
-        customer_checkin = roles[1][randint(0, 1)]
-        maintenance_crew = roles[2][randint(0, 1)]
-        flight_manager = roles[3][randint(0, 1)]
-        administrative_role = roles[4][randint(0, 2)]
+        return country[countryNumber]
     
-    def __init__(self) -> None:
-        pass 
+    def department(self,department_number):
+        departments = [
+            'In-Flight Department',
+            'Administrative Department',
+            'Customer Services Department',
+            'Maintaince Department',
+            'Scheduling and Safety Department'
+        ]
+        return departments[department_number]
+    
+    def job_title(self, department_number, role_number):
+        roles = [
+            ['Captain', 'First Officer', 'Second Officer', 'Flight Attendant'], # In-Flight Department
+            ['Finance Operator', 'Human Resources', 'Advertisement Manager'],   # Administrative Department
+            ['Check-in Personel', 'Customer Service'],                          # Customer Services Department
+            ['Inspector', 'Maintainer'],                                        # Maintaince Department
+            ['Flight Scheduling Manager', 'Payload Manager']                    # Scheduling and Safety Department
+        ]
+        return roles[department_number][role_number]
+    
+    def __init__(self):
+        self.department_number = randint(0, 4)
+        self.department = self.department(self.department_number)
+        
+        if self.department_number == 0:
+            self.jobTitle = self.job_title(0, randint(0, 3))
+        elif self.department_number == 1:
+            self.jobTitle = self.job_title(1, randint(0, 2))
+        else:
+            self.jobTitle = self.job_title(self.department_number, randint(0, 1))
+        
+        self.usageLocationNumber = randint(0, 2)
+        self.usageLocation = self.usageLocation(self.usageLocationNumber)
+
+    
+employee_info = basic_info().basic_info + employment_info().employment_info
+'''
+The employee info must be following this format:
+<----- Basic Info
+1. displayName
+2. userPrincipalName
+3. passwordProfile
+4. accountEnable
+5. givenName
+6. surname
+----->
+
+<------ emplyement_info
+7. jobTitle
+8. department
+9. usageLocation
+10. streetAddress
+11. state
+12. country
+13. physicalDeliveryOfficeName
+14. city
+15. postalCode
+16. telephoneNumber
+17. mobile
+----->
+'''

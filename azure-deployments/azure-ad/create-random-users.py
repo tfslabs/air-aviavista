1a47475e7192cb4191d7a8ce02daad784245fe79
ねこ 白い <shiroineko.tfs@gmail.com>
2024-03-13 20:37:11 +0700

    Update create-random-users.py

--- azure-deployments/azure-ad/create-random-users.py
+++ azure-deployments/azure-ad/create-random-users.py
@@ -1,14 +1,42 @@
+from os import system as cs
+from random import randint
 
-class personel_roles:
+cs('pip install names')
+import names, string, secrets
+
+class basic_info:
     
-    def __init__(self):
-        self.roles = [
+    def generate_password(self, length=randint(10, 16)):
+        # Define characters to use in the password
+        characters = string.ascii_letters + string.digits + string.punctuation
+        # Generate random password
+        password = ''.join(secrets.choice(characters) for _ in range(length))
+        return password
+    
+    def __init__(self) -> None:
+        self.first_name = names.get_first_name() # First Name
+        self.last_name = names.get_last_name() # Last Name
+        self.display_name = self.first_name + " " + self.last_name
+        self.email = self.first_name.lower() + self.last_name.lower() + "@tfslabs.onmicrosoft.com"
+        self.password = self.generate_password()
+        self.basic_info = [self.first_name, self.last_name, self.display_name, self.email, self.password]
+
+class employment_info:
+    
+    
+    def job_title(self):
+        roles = [
             ['Captain', 'First Officer', 'Second Officer', 'Flight Attendant'],
-            ['Check-in Personel', 'Payload Manager'],
+            ['Check-in Personel', 'Customer Service'],
             ['Inspector', 'Maintainer'],
-            ['Flight Scheduling Manager', 'Customer Service', 'Advertisement Manager'],
-            ['Finance Operator', 'Human Resources']
+            ['Flight Scheduling Manager', 'Payload Manager'],
+            ['Finance Operator', 'Human Resources', 'Advertisement Manager']
         ]
-        self.role_flight_attendants = self.roles[0]
-        self.role_ground_staff = self.roles[1]
-        
+        flight_attendants = roles[0][randint(0, 3)]
+        customer_checkin = roles[1][randint(0, 1)]
+        maintenance_crew = roles[2][randint(0, 1)]
+        flight_manager = roles[3][randint(0, 1)]
+        administrative_role = roles[4][randint(0, 2)]
+    
+    def __init__(self) -> None:
+        pass 
